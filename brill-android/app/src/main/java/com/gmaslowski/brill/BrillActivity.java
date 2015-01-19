16eafb62820a4fea70b425906deac56524a1dff2
Greg Maslowski <gregmaslowski@gmail.com>
2015-01-19 08:29:12 +0100

    Adding android project.

---
+++ brill-android/app/src/main/java/com/gmaslowski/brill/BrillActivity.java
@@ -0,0 +1,13 @@
+package com.gmaslowski.brill;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BrillActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brill);
+    }
+}
