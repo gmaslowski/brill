8a012c8feb6d99b1e9f42111e5747dba6b2f7607
Greg Maslowski <gregmaslowski@gmail.com>
2015-02-01 14:37:56 +0100

    Adding healthcheck for mongodb and apropriate test.

--- brill-core/app/controller/healthController.js
+++ brill-core/app/controller/healthController.js
@@ -1,6 +1,13 @@
+var mongoose = require('mongoose');
+
 module.exports = function (app) {
 
     app.get('/api/health', function (request, response) {
-        response.send('Hello World!');
+        var appState = {
+            brillRunning: 'OK',
+            mongodb: mongoose.connection.readyState === 1 ? 'OK' : 'NOT_OK'
+        };
+
+        response.send(appState);
     });
 };
--- brill-uat/src/test/groovy/com/gmaslowski/brill/uat/UATBasicSpecification.groovy
+++ brill-uat/src/test/groovy/com/gmaslowski/brill/uat/UATBasicSpecification.groovy
@@ -10,4 +10,12 @@ class UATBasicSpecification extends Specification {
     public static final String APPLICATION_JSON = "application/json"
 
     public static final String IDEAS_API = "/api/ideas"
+    public static final String HEALTH_API = "/api/health"
+
+    def setupSpec() {
+        def response = BRILL.get(path: HEALTH_API)
+
+        assert response.data.brillRunning == "OK"
+        assert response.data.mongodb == "OK"
+    }
 }
